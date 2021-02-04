a374c4aba0d66a70a59c6b432055f234f67f74e8
tak-eng <shibamoto2411@gmail.com>
2021-02-04 19:13:14 +0900

    settings.py修正

--- newsproject/settings.py
+++ newsproject/settings.py
@@ -13,11 +13,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
-from pathlib import Path
+# from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
